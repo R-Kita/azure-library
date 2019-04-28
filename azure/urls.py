174fbc9188e95b7844e32072cb85bc545e9d2643
Ryo Kita <r.kita.09@gmail.com>
2019-04-28 12:28:21 +0900

    Add post list page for curator.

--- azure/urls.py
+++ azure/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.novel_list, name='novel_list'),
     path('post/<int:pk>/', views.novel_detail, name='novel_detail'),
+    path('curate/', views.curate_novel_list, name='curate_novel_list'),
 ]
--- azure/views.py
+++ azure/views.py
@@ -7,6 +7,10 @@ def novel_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'azure/novel_list.html', {'posts': posts})
 
+def curate_novel_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'azure/novel_list.html', {'posts': posts, 'curate': 'valid'})
+
 def novel_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'azure/novel_detail.html', {'post': post})
