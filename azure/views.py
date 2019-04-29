f7e3f1b159a0460c5309d3ab4af71677a9a09e2a
Ryo Kita <r.kita.09@gmail.com>
2019-04-29 17:20:23 +0900

    Change listing novels from old-order to new-order.

--- azure/views.py
+++ azure/views.py
@@ -4,11 +4,11 @@ from .models import Post
 from .forms import PostForm
 
 def novel_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'azure/novel_list.html', {'posts': posts})
 
 def curate_novel_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(-'published_date')
     return render(request, 'azure/novel_list.html', {'posts': posts, 'curate': 'valid'})
 
 def novel_detail(request, pk):
