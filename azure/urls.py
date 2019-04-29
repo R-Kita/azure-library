6420b28e19b78ff72eee67ca0a1bae2e7cf53311
Ryo Kita <r.kita.09@gmail.com>
2019-04-29 15:18:04 +0900

    Add edit page but POST system.

--- azure/urls.py
+++ azure/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('novel/<int:pk>/', views.novel_detail, name='novel_detail'),
     path('curate/', views.curate_novel_list, name='curate_novel_list'),
     path('curate/novel/<int:pk>/', views.curate_novel_detail, name='curate_novel_detail'),
+    path('curate/novel/<int:pk>/edit/', views.curate_novel_edit, name='curate_novel_edit'),
 ]
--- azure/views.py
+++ azure/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-
+from .forms import PostForm
 
 def novel_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -19,3 +19,7 @@ def curate_novel_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'azure/novel_detail.html', {'post': post, 'curate': 'valid'})
 
+def curate_novel_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form  = PostForm(instance=post)
+    return render(request, 'azure/novel_edit.html', {'form': form, 'curate': 'valid'})
